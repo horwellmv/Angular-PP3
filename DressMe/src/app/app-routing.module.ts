1c4b69fca2e8ec2db186c20ae148351626a975d1
Fermin Tiscornia <fermintiscornia@gmail.com>
2023-09-07 11:00:46 -0300

    agregofooter

--- DressMe/src/app/app-routing.module.ts
+++ DressMe/src/app/app-routing.module.ts
@@ -19,7 +19,6 @@ const routes: Routes = [
   {path: 'login',component: LoginComponent},
   {path: 'admin-caja',component: AdminCajaComponent},
   {path: 'footer',component: FooterComponent},
-  /*{path: 'footerf',component: FooterComponentg},*/
   {path: 'admin-catalogo',component: AdminCatalogoComponent},
   {path: 'admin-perfiles',component: AdminPerfilesComponent},
   {path: 'admin-turnos',component: AdminTurnosComponent},
