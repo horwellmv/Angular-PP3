96841a367b9aeaa064bd441d8c226d8d4c5316ef
Horwell <horwellmv@gmail.com>
2023-10-22 00:58:48 -0300

    Se desarrolla metodo de logOut de Firebase.

--- DressMe/src/app/componentes/comunes/navbar/navbar.component.ts
+++ DressMe/src/app/componentes/comunes/navbar/navbar.component.ts
@@ -1,10 +1,21 @@
 import { Component } from '@angular/core';
-
+import { Router } from "@angular/router";
+import { AngularFireAuth } from '@angular/fire/compat/auth';
 @Component({
   selector: 'app-navbar',
   templateUrl: './navbar.component.html',
   styleUrls: ['./navbar.component.css']
 })
 export class NavbarComponent {
+  constructor(private auth: AngularFireAuth,private router: Router){}
+
+  logOut(){
+    console.log("funciona logout");
+    //localStorage.setItem("GuardianToken","");
+    //this.afAuth.signOut();
+    //this.router.navigate(['/login']);
+    this.router.navigate(['/login']);
+    this.auth.signOut();
+  }
 
 }
